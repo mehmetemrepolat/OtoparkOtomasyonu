bd5f2c91053b926d190d5629b6a2482908df2874
Emre <m.emrepolat@icloud.com>
2023-06-01 21:18:52 +0300

    new Form Parking History

---
+++ OtoparkOtomasyonu/parkingHistory.Designer.cs
@@ -0,0 +1,106 @@
+﻿using System.ComponentModel;
+
+namespace OtoparkOtomasyonu
+{
+    partial class parkingHistory
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(parkingHistory));
+            this.parking_view = new System.Windows.Forms.DataGridView();
+            this.customer_phone = new System.Windows.Forms.TextBox();
+            this.customer_name = new System.Windows.Forms.TextBox();
+            this.customer_car = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.parking_view)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.customer_car)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // parking_view
+            // 
+            this.parking_view.BackgroundColor = System.Drawing.SystemColors.ActiveBorder;
+            this.parking_view.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.DisableResizing;
+            this.parking_view.Location = new System.Drawing.Point(70, 66);
+            this.parking_view.Name = "parking_view";
+            this.parking_view.RowTemplate.Height = 24;
+            this.parking_view.Size = new System.Drawing.Size(561, 541);
+            this.parking_view.TabIndex = 0;
+            // 
+            // customer_phone
+            // 
+            this.customer_phone.Location = new System.Drawing.Point(948, 264);
+            this.customer_phone.Name = "customer_phone";
+            this.customer_phone.Size = new System.Drawing.Size(166, 22);
+            this.customer_phone.TabIndex = 3;
+            // 
+            // customer_name
+            // 
+            this.customer_name.Location = new System.Drawing.Point(948, 220);
+            this.customer_name.Name = "customer_name";
+            this.customer_name.Size = new System.Drawing.Size(166, 22);
+            this.customer_name.TabIndex = 5;
+            // 
+            // customer_car
+            // 
+            this.customer_car.Location = new System.Drawing.Point(676, 357);
+            this.customer_car.Name = "customer_car";
+            this.customer_car.Size = new System.Drawing.Size(480, 250);
+            this.customer_car.TabIndex = 6;
+            this.customer_car.TabStop = false;
+            // 
+            // parkingHistory
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1262, 673);
+            this.Controls.Add(this.customer_car);
+            this.Controls.Add(this.customer_name);
+            this.Controls.Add(this.customer_phone);
+            this.Controls.Add(this.parking_view);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "parkingHistory";
+            this.Text = "parkingHistory";
+            ((System.ComponentModel.ISupportInitialize)(this.parking_view)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.customer_car)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        private System.Windows.Forms.PictureBox customer_car;
+
+        private System.Windows.Forms.TextBox customer_name;
+
+        private System.Windows.Forms.TextBox customer_phone;
+
+        private System.Windows.Forms.DataGridView parking_view;
+
+        #endregion
+    }
+}
---
+++ OtoparkOtomasyonu/parkingHistory.cs
@@ -0,0 +1,133 @@
+﻿using System.Windows.Forms;
+using System;
+using MySql.Data.MySqlClient;
+using System.Data;
+using System.Drawing;
+using System.IO;
+
+namespace OtoparkOtomasyonu
+{
+    public partial class parkingHistory : Form
+    {
+        private string connectionString = "server=localhost;user=root;database=otopark-otomasyonu;password=413508;";
+        private string carsFolderPath = "Cars/";
+
+
+        public parkingHistory()
+        {
+            InitializeComponent();
+            ShowHistoryData();
+            parking_view.SelectionChanged += Parking_view_SelectionChanged;
+            
+            customer_car.SizeMode = PictureBoxSizeMode.StretchImage;
+
+
+        }
+
+        private void Parking_view_SelectionChanged(object sender, EventArgs e)
+        {
+            if (parking_view.SelectedCells.Count > 0)
+            {
+                int selectedRowIndex = parking_view.SelectedCells[0].RowIndex;
+                DataGridViewRow selectedRow = parking_view.Rows[selectedRowIndex];
+                string customerPlate = selectedRow.Cells["customer_plate"].Value.ToString();
+                string imagePath = Path.Combine(carsFolderPath, customerPlate + ".png");
+
+                if (File.Exists(imagePath))
+                {
+                    customer_car.Image = Image.FromFile(imagePath);
+                }
+                else
+                {
+                    // İlgili dosya bulunamazsa varsayılan bir görüntü atanabilir veya boş bırakılabilir.
+                    customer_car.Image = null;
+                }
+                GetCustomerDetails(customerPlate);
+
+            }
+        }
+
+        private void GetCustomerDetails(string customerPlate)
+        {
+             try
+            {
+                using (MySqlConnection connection = new MySqlConnection(connectionString))
+                {
+                    connection.Open();
+
+                    string query = "SELECT customer_name, customer_phone FROM customer WHERE customer_plate = @customerPlate";
+
+                    using (MySqlCommand command = new MySqlCommand(query, connection))
+                    {
+                        command.Parameters.AddWithValue("@customerPlate", customerPlate);
+
+                        using (MySqlDataReader reader = command.ExecuteReader())
+                        {
+                            if (reader.Read())
+                            {
+                                string customerName = reader["customer_name"].ToString();
+                                string customerPhone = reader["customer_phone"].ToString();
+
+                                customer_name.Text = customerName;
+                                customer_phone.Text = customerPhone;
+                            }
+                            else
+                            {
+                                // İlgili plakaya sahip müşteri bulunamadıysa, textboxları boş bırakabilirsiniz veya isteğe bağlı bir işlem yapabilirsiniz.
+                                customer_name.Text = string.Empty;
+                                customer_phone.Text = string.Empty;
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+
+        private void ShowHistoryData()
+        {
+            try
+            {
+                using (MySqlConnection connection = new MySqlConnection(connectionString))
+                {
+                    connection.Open();
+
+                    string query = "SELECT customer_plate, parking_date, parked_time FROM parkinghistory";
+
+                    using (MySqlCommand command = new MySqlCommand(query, connection))
+                    {
+                        using (MySqlDataAdapter adapter = new MySqlDataAdapter(command))
+                        {
+                            DataTable dataTable = new DataTable();
+                            adapter.Fill(dataTable);
+                            
+                            parking_view.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+                            parking_view.DataSource = dataTable;
+                            parking_view.EditMode = DataGridViewEditMode.EditProgrammatically;
+                            parking_view.AllowUserToResizeRows = false;
+                            parking_view.AllowUserToResizeColumns = false;
+                            parking_view.RowHeadersVisible = false;
+                            
+              
+                            customer_car.BackColor = Color.Transparent;
+
+
+                        }
+                    }
+
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+
+
+    }
+}
